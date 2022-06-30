54cedd8a02415eca4c21e8dbc9e7d0c993b1fe85
davidpengiupui <90153025+davidpengiupui@users.noreply.github.com>
2022-06-30 15:20:46 -0400

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,5 +2,6 @@ from .resnet import *
 from .ibn import *
 from .digits import *
 from .hrnet import get_pose_net
+from .tokenpose import get_tokenpose_B, get_tokenpose_L
 
 __all__ = ['resnet', 'digits', 'ibn']
