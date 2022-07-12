1e83d7358aa341942d8183d9362233d43965da57
davidpengiupui <90153025+davidpengiupui@users.noreply.github.com>
2022-07-11 20:03:43 -0400

    Update loss.py

--- loss.py
+++ loss.py
@@ -8,6 +8,45 @@ import torch.nn as nn
 import torch.nn.functional as F
 from .metric import get_max_preds_torch
 
+def guassian_kernel(source, target, kernel_mul=2.0, kernel_num=5, fix_sigma=None):
+    n_samples = int(source.size()[0])+int(target.size()[0])
+    total = torch.cat([source, target], dim=0)
+    total0 = total.unsqueeze(0).expand(int(total.size(0)), int(total.size(0)), int(total.size(1)))
+    total1 = total.unsqueeze(1).expand(int(total.size(0)), int(total.size(0)), int(total.size(1)))
+    L2_distance = ((total0-total1)**2).sum(2)
+    if fix_sigma:
+        bandwidth = fix_sigma
+    else:
+        bandwidth = torch.sum(L2_distance.data) / (n_samples**2-n_samples)
+    bandwidth /= kernel_mul ** (kernel_num // 2)
+    bandwidth_list = [bandwidth * (kernel_mul**i) for i in range(kernel_num)]
+    kernel_val = [torch.exp(-L2_distance / bandwidth_temp) for bandwidth_temp in bandwidth_list]
+    return sum(kernel_val)
+
+def mmd(source, target, kernel_mul=2.0, kernel_num=5, fix_sigma=None):
+    batch_size = int(source.size()[0])
+    kernels = guassian_kernel(source, target,
+                              kernel_mul=kernel_mul, kernel_num=kernel_num, fix_sigma=fix_sigma)
+    XX = kernels[:batch_size, :batch_size]
+    YY = kernels[batch_size:, batch_size:]
+    XY = kernels[:batch_size, batch_size:]
+    YX = kernels[batch_size:, :batch_size]
+    loss = torch.mean(XX + YY - XY -YX)
+    return loss
+
+def mmd_loss(pred_hm, gt_hm, weight):
+    B, C, H, W = pred_hm.shape
+    loss = torch.tensor(0.)
+    for i in range(B):
+        for j in range(C):
+            pred = pred_hm[i,j,:,:].flatten()
+            gt = gt_hm[i,j,:,:].flatten()
+            loss += weight[i,j,:] * mmd(pred, gt)
+            
+    loss /= B*C
+    
+    return loss
+
 def oks_loss(pred_hm, gt_hm, weight, num_keypoints):
     sigmas = torch.Tensor(np.array([1.] * num_keypoints))
     var = (sigmas * 2) ** 2
